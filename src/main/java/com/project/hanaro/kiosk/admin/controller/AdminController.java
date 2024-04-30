8dcda246394eb09d294e67d1922486f821667a5e
duddn2012 <gowoo123@naver.com>
2024-04-30 16:16:44 +0900

    thymeleaf setting

---
+++ src/main/java/com/project/hanaro/kiosk/admin/controller/AdminController.java
@@ -0,0 +1,23 @@
+package com.project.hanaro.kiosk.admin.controller;
+
+import com.project.hanaro.kiosk.admin.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final AdminService adminService;
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "index";
+    }
+
+    @GetMapping("/admin/member")
+    public String adminMemberPage(){
+        return "/admin/member_management";
+    }
+}
---
+++ src/main/java/com/project/hanaro/kiosk/admin/service/AdminService.java
@@ -0,0 +1,10 @@
+package com.project.hanaro.kiosk.admin.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+
+}
