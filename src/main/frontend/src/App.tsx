f9cb9d16c3d15a64b0ce45c04fb3958566afeba5
khw18 <gmsb14@naver.com>
2024-05-08 11:20:14 +0900

    feat: guest login join front

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -5,12 +5,14 @@ import Place from "./pages/Place";
 import Cart from "./pages/Cart";
 
 import Login from "./pages/Login";
+import Join from "./pages/Join";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/Join" element={<Join />} />
       <Route path="/place" element={<Place />} />
       <Route path="/order" element={<Cart />} />
     </Routes>
