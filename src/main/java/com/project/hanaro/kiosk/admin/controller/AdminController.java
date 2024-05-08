28ecdfb9290bbde4f1803aa3bcd732f9189fab14
duddn2012 <gowoo123@naver.com>
2024-05-08 14:46:22 +0900

    feat: products admin update page

--- src/main/java/com/project/hanaro/kiosk/admin/controller/AdminController.java
+++ src/main/java/com/project/hanaro/kiosk/admin/controller/AdminController.java
@@ -1,15 +1,20 @@
 package com.project.hanaro.kiosk.admin.controller;
 
 import com.project.hanaro.kiosk.admin.service.AdminService;
+import com.project.hanaro.kiosk.products.dto.ProductGetResponse;
+import com.project.hanaro.kiosk.products.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
 public class AdminController {
 
     private final AdminService adminService;
+    private final ProductService productService;
 
     @GetMapping("/admin")
     public String adminPage() {
@@ -24,6 +29,13 @@ public class AdminController {
     @GetMapping("/admin/product")
     public String adminProductPage(){
         return "/admin/product_management";
+    }
+
+    @GetMapping("/admin/product/prod-upd/{productId}")
+    public String adminProductUpdatePage(@PathVariable Long productId, Model model){
+        ProductGetResponse product = productService.findProduct(productId);
+        model.addAttribute("product", product);
+        return "/admin/product_management_update";
     }
 
     @GetMapping("/admin/order")
